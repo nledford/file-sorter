6980b2ed178255238f6be074a813fdb58584358e
Nathaniel Ledford <nate@nateledford.com>
2020-10-25 09:55:02 -0400

    Refactor CLI options

--- src/app.rs
+++ src/app.rs
@@ -3,6 +3,22 @@ use clap::Clap;
 #[derive(Clap)]
 #[clap(version = "1.0", author = "Nathaniel Ledford <nate@nateledford.com>")]
 pub struct Opts {
-    /// The directory of files to be organized
+    #[clap(subcommand)]
+    pub subcmd: SubCommand,
+}
+
+#[derive(Clap)]
+pub enum SubCommand {
+    Sort(Sort),
+    Append(Append),
+}
+
+#[derive(Clap)]
+pub struct Sort {
+    pub dir: String,
+}
+
+#[derive(Clap)]
+pub struct Append {
     pub dir: String,
 }
